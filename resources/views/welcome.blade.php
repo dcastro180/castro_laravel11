4f45f277f3adc9f859973db7d9a9449bc4eaf25b
Diogo Castro <dcastrocs@gmail.com>
2024-08-14 20:09:19 -0300

    Primeiro commit do Projeto Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Diogo Castro</title>
+    </head>
+    <body>
+        <div>
+            <h1>Bem Vindo </h1>
+            <p>Data Atual: {{\carbon\carbon:: now() -> format('d/m/Y H:i:s') }}</p><br>
+            <p>Primeiro teste no Laravel 11</p>
+        </div>
+    </body>
+</html>
